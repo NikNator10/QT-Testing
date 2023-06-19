c1865ed2c6ede42b94fca44cc972eaa91a5e3532
NikNator10 <niknator1001@gmail.com>
2023-06-19 13:32:12 +0200

    Neues Wissen angewendet: Umgang mit QLabel und QLineEdit, vertikale Layouts einbauen, ToolBar und MenuBar einfügen; Genutzt: QApplication, QWidget, QMenuBar, QToolBar, QLabel, QLineEdit, QVBoxLayout, QPushButton, QMessageBox

--- forms.py
+++ forms.py
@@ -1,80 +1,70 @@
-from PySide6.QtWidgets import QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QLineEdit, QLabel, QMenuBar, QToolBar, QMessageBox
-from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QWidget, QMenuBar, QToolBar, QLabel, QLineEdit, QVBoxLayout, QPushButton, QMessageBox
+
 class DataForm(QWidget):
     def __init__(self, app):
         super().__init__()
-
         self.app = app
-        self.setWindowTitle("DataForm")
+        self.setWindowTitle("Data Collector")
 
-        # Creating a MenuBar
         menu_bar = QMenuBar()
         file_menu = menu_bar.addMenu("File")
-        about_action = file_menu.addAction("About")
-        about_action.triggered.connect(self.about_msgbox)
         quit_action = file_menu.addAction("Quit")
-        quit_action.triggered.connect(self.quit_action)
+        quit_action.setShortcut("Ctrl+q")
+        about_action = file_menu.addAction("About")
+        about_action.setShortcut("Ctrl+u")
 
-        # Creating a ToolBar
         tool_bar = QToolBar()
         tool_bar.addAction(quit_action)
-        tool_bar.addSeparator()
+        tool_bar.addAction(about_action)
 
-        # Creates Labels, LineEdits and Silder 
+        quit_action.triggered.connect(self.quit_action)
+        about_action.triggered.connect(self.about_action)
+
         label_fname = QLabel("First Name: ")
-        self.line_edit_fname = QLineEdit()
-        
+        self.line_fname = QLineEdit()
+
         label_lname = QLabel("Last Name: ")
-        self.line_edit_lname = QLineEdit()
+        self.line_lname = QLineEdit()
 
         label_address = QLabel("Address: ")
-        self.line_edit_address = QLineEdit()
+        self.line_address = QLineEdit()
+        self.line_address.returnPressed.connect(self.send_input)
+
 
         send_button = QPushButton("Send")
-        send_button.released.connect(self.data_input)
-
-        self.data_status = QLabel("Waiting for Data...")
-
-        # Adds Lables and LineEdits to horizontal layouts
-        h_layout_menubar = QHBoxLayout()
-        h_layout_menubar.addWidget(menu_bar)
+        send_button.clicked.connect(self.send_input)
 
 
-        h_layout_fname = QHBoxLayout()
-        h_layout_fname.addWidget(label_fname)
-        h_layout_fname.addWidget(self.line_edit_fname)
+        self.label_status = QLabel("Status: ")
 
-        h_layout_lname = QHBoxLayout()
-        h_layout_lname.addWidget(label_lname)
-        h_layout_lname.addWidget(self.line_edit_lname)
-
-        h_layout_address = QHBoxLayout()
-        h_layout_address.addWidget(label_address)
-        h_layout_address.addWidget(self.line_edit_address)
-
-        # Combines horizontal layouts and buttons to a big vertical layout
         v_layout = QVBoxLayout()
-        v_layout.addLayout(h_layout_menubar)
+        v_layout.addWidget(menu_bar)
         v_layout.addWidget(tool_bar)
-        v_layout.addLayout(h_layout_fname)
-        v_layout.addLayout(h_layout_lname)
-        v_layout.addLayout(h_layout_address)
+        v_layout.addWidget(label_fname)
+        v_layout.addWidget(self.line_fname)
+        v_layout.addWidget(label_lname)
+        v_layout.addWidget(self.line_lname)
+        v_layout.addWidget(label_address)
+        v_layout.addWidget(self.line_address)
         v_layout.addWidget(send_button)
-        v_layout.addWidget(self.data_status)
+        v_layout.addWidget(self.label_status)
 
         self.setLayout(v_layout)
-    
 
-    def data_input(self):
-        QMessageBox.information(self, "Data", "Data sent!", QMessageBox.Ok)
-        print("Data collected successfully!")
-        print("First Name: ", self.line_edit_fname.text())
-        print("Last Name: ", self.line_edit_lname.text())
-        print("Address:", self.line_edit_address.text())
-        self.data_status.setText("Data received!")
+
+
+
+
+    def send_input(self):
+        print("Data collected:")
+        print("First Name: ", self.line_fname.text())
+        print("Last Name: ", self.line_lname.text())
+        print("Address: ", self.line_address.text())
+        self.label_status.setText("Status:\nData submitted!")
 
     def quit_action(self, app):
         self.app.quit()
-    
-    def about_msgbox(self):
-        QMessageBox.about(self,"About", "This is a little data collector" )
+
+    def about_action(self):
+        QMessageBox.about(self, "About", "This is a data collector")
+
