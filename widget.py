3c999dc1dae1387dd12fe9c1fd3277f5dc46b77b
NikNator10 <niknator1001.@gmail.com>
2023-08-22 00:49:24 +0200

    Gelernt: QFontDialog; Genutzt: QApplication, QWidget, QFontDialog, QFont, Qt Designer

--- widget.py
+++ widget.py
@@ -1,39 +1,18 @@
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QPushButton, QFileDialog
+from PySide6.QtWidgets import QWidget, QFontDialog
+from PySide6.QtGui import QFont
+from ui_widget import Ui_Widget
 
-class Widget(QWidget):
+class Widget(QWidget, Ui_Widget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("QFileDialog - Demo")
+        self.setupUi(self)
+        self.setWindowTitle("Font - Selector")
 
-        self.resize(300,100)
+        self.button.clicked.connect(self.select_font)
 
-        get_existing_dir_button = QPushButton("getExistingDirectory")
-        get_open_file_name = QPushButton("getOpenFileName")
-        get_open_file_names = QPushButton("getOpenFileNames")
-        get_save_file_name = QPushButton("getSaveFileName")
-
-        get_existing_dir_button.clicked.connect(self.get_dir)
-        get_open_file_name.clicked.connect(self.get_open_file_name)
-        get_open_file_names.clicked.connect(self.get_open_file_names)
-        get_save_file_name.clicked.connect(self.get_save_file_name)
-
-        v_layout = QVBoxLayout()
-        v_layout.addWidget(get_existing_dir_button)
-        v_layout.addWidget(get_open_file_name)
-        v_layout.addWidget(get_open_file_names)
-        v_layout.addWidget(get_save_file_name)
-        self.setLayout(v_layout)
-
-    def get_dir(self):
-        dir = QFileDialog.getExistingDirectory(self, "Open Directory", "", QFileDialog.ShowDirsOnly | QFileDialog.DontResolveSymlinks)
-        print(dir)
-    def get_open_file_name(self):
-        file_name,_ = QFileDialog.getOpenFileName(self, "Open File", "/home", "Images (*.png *.jpg);;All Files (*.*)")
-        print(file_name)
-    def get_open_file_names(self):
-        file_names,_ = QFileDialog.getOpenFileNames(self, "Open Files", "/home", "Images (*.png *.jpg);;All Files (*.*)")
-        for f in file_names:
-            print(f)
-    def get_save_file_name(self):
-        file_name,_ = QFileDialog.getSaveFileName(self, "Save File", "/home", "Images (*.png *.jpg);;All Files (*.*)")
-        print(file_name)
+    def select_font(self):
+        ok,font = QFontDialog.getFont(QFont("Arial", 12), self)
+        if ok:
+            self.label.setFont(font)
+        else:
+            print("No font selected")
