a8f0e0d6a614e711d9decbab23460b148aaae223
NikNator10 <niknator1001.@gmail.com>
2023-08-23 16:25:09 +0200

    Gelernt: Text-/Hintergrundfarben verändern mit QColorDialog; Wiederholung: QFontDialog; Genutzt: QApplication, QWidget, QFontDialog, QColorDialog, QPalette, QFont, Qt Designer

--- ui_widget.py
+++ ui_widget.py
@@ -15,20 +15,35 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QLabel, QPushButton, QSizePolicy,
-    QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QLabel, QPushButton,
+    QSizePolicy, QVBoxLayout, QWidget)
 
 class Ui_Widget(object):
     def setupUi(self, Widget):
         if not Widget.objectName():
             Widget.setObjectName(u"Widget")
-        Widget.resize(460, 102)
+        Widget.resize(519, 85)
         self.verticalLayout = QVBoxLayout(Widget)
         self.verticalLayout.setObjectName(u"verticalLayout")
-        self.button = QPushButton(Widget)
-        self.button.setObjectName(u"button")
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.text_color_button = QPushButton(Widget)
+        self.text_color_button.setObjectName(u"text_color_button")
 
-        self.verticalLayout.addWidget(self.button)
+        self.horizontalLayout.addWidget(self.text_color_button)
+
+        self.background_color_button = QPushButton(Widget)
+        self.background_color_button.setObjectName(u"background_color_button")
+
+        self.horizontalLayout.addWidget(self.background_color_button)
+
+        self.font_button = QPushButton(Widget)
+        self.font_button.setObjectName(u"font_button")
+
+        self.horizontalLayout.addWidget(self.font_button)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
 
         self.label = QLabel(Widget)
         self.label.setObjectName(u"label")
@@ -43,7 +58,9 @@ class Ui_Widget(object):
 
     def retranslateUi(self, Widget):
         Widget.setWindowTitle(QCoreApplication.translate("Widget", u"Form", None))
-        self.button.setText(QCoreApplication.translate("Widget", u"Click to change font", None))
-        self.label.setText(QCoreApplication.translate("Widget", u"Test font", None))
+        self.text_color_button.setText(QCoreApplication.translate("Widget", u"Text Color", None))
+        self.background_color_button.setText(QCoreApplication.translate("Widget", u"Background Color", None))
+        self.font_button.setText(QCoreApplication.translate("Widget", u"Font", None))
+        self.label.setText(QCoreApplication.translate("Widget", u"Some text", None))
     # retranslateUi
 
--- widget.py
+++ widget.py
@@ -1,18 +1,43 @@
-from PySide6.QtWidgets import QWidget, QFontDialog
-from PySide6.QtGui import QFont
+from PySide6.QtWidgets import QWidget, QFontDialog, QColorDialog
+from PySide6.QtGui import QPalette, QFont
 from ui_widget import Ui_Widget
 
 class Widget(QWidget, Ui_Widget):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.setWindowTitle("Font - Selector")
+        self.setWindowTitle("Change your text/background color & font")
+        self.label.setAutoFillBackground(True)
 
-        self.button.clicked.connect(self.select_font)
+        self.text_color_button.clicked.connect(self.set_text_color)
+        self.background_color_button.clicked.connect(self.set_background_color)
+        self.font_button.clicked.connect(self.set_font)
 
-    def select_font(self):
-        ok,font = QFontDialog.getFont(QFont("Arial", 12), self)
+    def set_text_color(self):
+        palette = self.label.palette()
+        color = palette.color(QPalette.WindowText)
+        chosenColor = QColorDialog.getColor(color, self, "Choose text color")
+
+        if chosenColor.isValid():
+            palette.setColor(QPalette.WindowText, chosenColor)
+            self.label.setPalette(palette)
+        else:
+            print("No vaild color selected")
+    
+    def set_background_color(self):
+        palette = self.label.palette()
+        color = palette.color(QPalette.Window)
+        chosenColor = QColorDialog.getColor(color, self, "Choose background color")
+
+        if chosenColor.isValid():
+            palette.setColor(QPalette.Window, chosenColor)
+            self.label.setPalette(palette)
+        else:
+            print("No valid color selected")
+    
+    def set_font(self):
+        ok,font = QFontDialog.getFont(QFont("Arial", 20), self)
         if ok:
             self.label.setFont(font)
         else:
-            print("No font selected")
+            print("No valid font selected")
