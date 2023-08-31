bf6d175c937b449155ad3f39fb8e00410ddf0858
NikNator10 <niknator1001.@gmail.com>
2023-08-31 22:11:42 +0200

    Wiederholung: Farben mit QSettings speichern, QColorDialog; Genutzt: QApplication, QWidget, Qt, QColorDialog, QColor, QSettings, Qt Designer

--- ui_widget.py
+++ ui_widget.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'widget.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.5.1
+## Created by: Qt User Interface Compiler version 6.5.2
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -15,96 +15,87 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QGridLayout, QPushButton, QSizePolicy,
-    QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QGridLayout, QHBoxLayout, QPushButton,
+    QSizePolicy, QVBoxLayout, QWidget)
 
 class Ui_Widget(object):
     def setupUi(self, Widget):
         if not Widget.objectName():
             Widget.setObjectName(u"Widget")
-        Widget.resize(552, 457)
-        self.verticalLayout = QVBoxLayout(Widget)
-        self.verticalLayout.setObjectName(u"verticalLayout")
-        self.save_color_button = QPushButton(Widget)
-        self.save_color_button.setObjectName(u"save_color_button")
-
-        self.verticalLayout.addWidget(self.save_color_button)
-
-        self.load_color_button = QPushButton(Widget)
-        self.load_color_button.setObjectName(u"load_color_button")
-
-        self.verticalLayout.addWidget(self.load_color_button)
-
+        Widget.resize(514, 495)
+        self.horizontalLayout = QHBoxLayout(Widget)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.gridLayout = QGridLayout()
         self.gridLayout.setObjectName(u"gridLayout")
-        self.button_2 = QPushButton(Widget)
-        self.button_2.setObjectName(u"button_2")
-        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.button_1 = QPushButton(Widget)
+        self.button_1.setObjectName(u"button_1")
+        sizePolicy = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.button_1.sizePolicy().hasHeightForWidth())
+        self.button_1.setSizePolicy(sizePolicy)
+
+        self.gridLayout.addWidget(self.button_1, 0, 0, 1, 1)
+
+        self.button_2 = QPushButton(Widget)
+        self.button_2.setObjectName(u"button_2")
         sizePolicy.setHeightForWidth(self.button_2.sizePolicy().hasHeightForWidth())
         self.button_2.setSizePolicy(sizePolicy)
 
-        self.gridLayout.addWidget(self.button_2, 1, 0, 1, 1)
-
-        self.button_7 = QPushButton(Widget)
-        self.button_7.setObjectName(u"button_7")
-        sizePolicy.setHeightForWidth(self.button_7.sizePolicy().hasHeightForWidth())
-        self.button_7.setSizePolicy(sizePolicy)
-
-        self.gridLayout.addWidget(self.button_7, 2, 0, 1, 1)
+        self.gridLayout.addWidget(self.button_2, 0, 1, 1, 1)
 
         self.button_3 = QPushButton(Widget)
         self.button_3.setObjectName(u"button_3")
         sizePolicy.setHeightForWidth(self.button_3.sizePolicy().hasHeightForWidth())
         self.button_3.setSizePolicy(sizePolicy)
 
-        self.gridLayout.addWidget(self.button_3, 0, 1, 1, 1)
+        self.gridLayout.addWidget(self.button_3, 0, 2, 1, 1)
 
         self.button_4 = QPushButton(Widget)
         self.button_4.setObjectName(u"button_4")
         sizePolicy.setHeightForWidth(self.button_4.sizePolicy().hasHeightForWidth())
         self.button_4.setSizePolicy(sizePolicy)
 
-        self.gridLayout.addWidget(self.button_4, 1, 1, 1, 1)
-
-        self.button_1 = QPushButton(Widget)
-        self.button_1.setObjectName(u"button_1")
-        sizePolicy.setHeightForWidth(self.button_1.sizePolicy().hasHeightForWidth())
-        self.button_1.setSizePolicy(sizePolicy)
-
-        self.gridLayout.addWidget(self.button_1, 0, 0, 1, 1)
+        self.gridLayout.addWidget(self.button_4, 1, 0, 1, 1)
 
         self.button_5 = QPushButton(Widget)
         self.button_5.setObjectName(u"button_5")
         sizePolicy.setHeightForWidth(self.button_5.sizePolicy().hasHeightForWidth())
         self.button_5.setSizePolicy(sizePolicy)
 
-        self.gridLayout.addWidget(self.button_5, 0, 2, 1, 2)
-
-        self.button_9 = QPushButton(Widget)
-        self.button_9.setObjectName(u"button_9")
-        sizePolicy.setHeightForWidth(self.button_9.sizePolicy().hasHeightForWidth())
-        self.button_9.setSizePolicy(sizePolicy)
-
-        self.gridLayout.addWidget(self.button_9, 2, 3, 1, 1)
+        self.gridLayout.addWidget(self.button_5, 1, 1, 1, 1)
 
         self.button_6 = QPushButton(Widget)
         self.button_6.setObjectName(u"button_6")
         sizePolicy.setHeightForWidth(self.button_6.sizePolicy().hasHeightForWidth())
         self.button_6.setSizePolicy(sizePolicy)
 
-        self.gridLayout.addWidget(self.button_6, 1, 2, 1, 2)
-
-        self.button_8 = QPushButton(Widget)
-        self.button_8.setObjectName(u"button_8")
-        sizePolicy.setHeightForWidth(self.button_8.sizePolicy().hasHeightForWidth())
-        self.button_8.setSizePolicy(sizePolicy)
-
-        self.gridLayout.addWidget(self.button_8, 2, 1, 1, 2)
+        self.gridLayout.addWidget(self.button_6, 1, 2, 1, 1)
 
 
-        self.verticalLayout.addLayout(self.gridLayout)
+        self.horizontalLayout.addLayout(self.gridLayout)
+
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.save_color_button = QPushButton(Widget)
+        self.save_color_button.setObjectName(u"save_color_button")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.save_color_button.sizePolicy().hasHeightForWidth())
+        self.save_color_button.setSizePolicy(sizePolicy1)
+
+        self.verticalLayout.addWidget(self.save_color_button)
+
+        self.load_color_button = QPushButton(Widget)
+        self.load_color_button.setObjectName(u"load_color_button")
+        sizePolicy1.setHeightForWidth(self.load_color_button.sizePolicy().hasHeightForWidth())
+        self.load_color_button.setSizePolicy(sizePolicy1)
+
+        self.verticalLayout.addWidget(self.load_color_button)
+
+
+        self.horizontalLayout.addLayout(self.verticalLayout)
 
 
         self.retranslateUi(Widget)
@@ -114,16 +105,13 @@ class Ui_Widget(object):
 
     def retranslateUi(self, Widget):
         Widget.setWindowTitle(QCoreApplication.translate("Widget", u"Form", None))
+        self.button_1.setText(QCoreApplication.translate("Widget", u"Button 1", None))
+        self.button_2.setText(QCoreApplication.translate("Widget", u"Button 2", None))
+        self.button_3.setText(QCoreApplication.translate("Widget", u"Button 3", None))
+        self.button_4.setText(QCoreApplication.translate("Widget", u"Button 4", None))
+        self.button_5.setText(QCoreApplication.translate("Widget", u"Button 5", None))
+        self.button_6.setText(QCoreApplication.translate("Widget", u"Button 6", None))
         self.save_color_button.setText(QCoreApplication.translate("Widget", u"Save Color", None))
         self.load_color_button.setText(QCoreApplication.translate("Widget", u"Load Color", None))
-        self.button_2.setText(QCoreApplication.translate("Widget", u"Button 4", None))
-        self.button_7.setText(QCoreApplication.translate("Widget", u"Button 7", None))
-        self.button_3.setText(QCoreApplication.translate("Widget", u"Button 2", None))
-        self.button_4.setText(QCoreApplication.translate("Widget", u"Button 5", None))
-        self.button_1.setText(QCoreApplication.translate("Widget", u"Button 1", None))
-        self.button_5.setText(QCoreApplication.translate("Widget", u"Button 3", None))
-        self.button_9.setText(QCoreApplication.translate("Widget", u"Button 9", None))
-        self.button_6.setText(QCoreApplication.translate("Widget", u"Button 6", None))
-        self.button_8.setText(QCoreApplication.translate("Widget", u"Button 8", None))
     # retranslateUi
 
--- widget.py
+++ widget.py
@@ -1,15 +1,14 @@
+from PySide6.QtCore import Qt, QSettings
+from PySide6.QtGui import QColor
 from PySide6.QtWidgets import QWidget, QColorDialog
-from PySide6.QtCore import QSettings
-from PySide6.QtGui import Qt, QColor
 from ui_widget import Ui_Widget
 
 class Widget(QWidget, Ui_Widget):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.setWindowTitle("QSettings - Demo")
 
-        self.color_list = [Qt.black, Qt.black, Qt.black, Qt.black, Qt.black, Qt.black, Qt.black, Qt.black, Qt.black]
+        self.color_list = [Qt.black, Qt.black, Qt.black, Qt.black, Qt.black, Qt.black]
 
         self.button_1.clicked.connect(self.button_1_clicked)
         self.button_2.clicked.connect(self.button_2_clicked)
@@ -17,117 +16,79 @@ class Widget(QWidget, Ui_Widget):
         self.button_4.clicked.connect(self.button_4_clicked)
         self.button_5.clicked.connect(self.button_5_clicked)
         self.button_6.clicked.connect(self.button_6_clicked)
-        self.button_7.clicked.connect(self.button_7_clicked)
-        self.button_8.clicked.connect(self.button_8_clicked)
-        self.button_9.clicked.connect(self.button_9_clicked)
 
         self.save_color_button.clicked.connect(self.save_color_button_clicked)
         self.load_color_button.clicked.connect(self.load_color_button_clicked)
 
     def button_1_clicked(self):
-        color = QColorDialog.getColor(self.color_list[0], self, "Choose a color")
+        color = QColorDialog.getColor(self.color_list[0], self, "Choose a background color")
         if color.isValid():
             self.color_list[0] = color
             css = "background-color:" + color.name()
             self.button_1.setStyleSheet(css)
     
     def button_2_clicked(self):
-        color = QColorDialog.getColor(self.color_list[1], self, "Choose a color")
+        color = QColorDialog.getColor(self.color_list[1], self, "Choose a background color")
         if color.isValid():
             self.color_list[1] = color
             css = "background-color:" + color.name()
             self.button_2.setStyleSheet(css)
 
     def button_3_clicked(self):
-        color = QColorDialog.getColor(self.color_list[2], self, "Choose a color")
+        color = QColorDialog.getColor(self.color_list[2], self, "Choose a background color")
         if color.isValid():
             self.color_list[2] = color
             css = "background-color:" + color.name()
             self.button_3.setStyleSheet(css)
     
     def button_4_clicked(self):
-        color = QColorDialog.getColor(self.color_list[3], self, "Choose a color")
+        color = QColorDialog.getColor(self.color_list[3], self, "Choose a background color")
         if color.isValid():
             self.color_list[3] = color
             css = "background-color:" + color.name()
             self.button_4.setStyleSheet(css)
-
+    
     def button_5_clicked(self):
-        color = QColorDialog.getColor(self.color_list[4], self, "Choose a color")
+        color = QColorDialog.getColor(self.color_list[4], self, "Choose a background color")
         if color.isValid():
             self.color_list[4] = color
             css = "background-color:" + color.name()
             self.button_5.setStyleSheet(css)
     
     def button_6_clicked(self):
-        color = QColorDialog.getColor(self.color_list[5], self, "Choose a color")
+        color = QColorDialog.getColor(self.color_list[5], self, "Choose a background color")
         if color.isValid():
             self.color_list[5] = color
             css = "background-color:" + color.name()
             self.button_6.setStyleSheet(css)
 
-    def button_7_clicked(self):
-        color = QColorDialog.getColor(self.color_list[6], self, "Choose a color")
-        if color.isValid():
-            self.color_list[6] = color
-            css = "background-color:" + color.name()
-            self.button_7.setStyleSheet(css)
-
-    def button_8_clicked(self):
-        color = QColorDialog.getColor(self.color_list[8], self, "Choose a color")
-        if color.isValid():
-            self.color_list[7] = color
-            css = "background-color:" + color.name()
-            self.button_8.setStyleSheet(css)
-    
-    def button_9_clicked(self):
-        color = QColorDialog.getColor(self.color_list[8], self, "Choose a color")
-        if color.isValid():
-            self.color_list[8] = color
-            css = "background-color:" + color.name()
-            self.button_9.setStyleSheet(css)
-    
-    def save_color(self, key, color):
+    def save_color(self, key, color): 
         l_color = QColor(color)
-        red = l_color.red()
-        green = l_color.green()
-        blue = l_color.blue()
+        red = l_color.red() 
+        green =l_color.green() 
+        blue = l_color.blue() 
 
         settings = QSettings("Your Company Name", "Your Product Name")
-        settings.beginGroup("Button Color")
+        settings.beginGroup("ButtonColor")
         settings.setValue(key + "r", red)
         settings.setValue(key + "g", green)
         settings.setValue(key + "b", blue)
         settings.endGroup()
 
-
-    def save_color_button_clicked(self):
-        self.save_color("button_1", self.color_list[0])
-        self.save_color("button_2", self.color_list[1])
-        self.save_color("button_3", self.color_list[2])
-        self.save_color("button_4", self.color_list[3])
-        self.save_color("button_5", self.color_list[4])
-        self.save_color("button_6", self.color_list[5])
-        self.save_color("button_7", self.color_list[6])
-        self.save_color("button_8", self.color_list[7])
-        self.save_color("button_9", self.color_list[8])
-
     def load_color(self, key):
         settings = QSettings("Your Company Name", "Your Product Name")
-        settings.beginGroup("Button Color")
+        settings.beginGroup("ButtonColor")
         red = settings.value(key + "r")
         green = settings.value(key + "g")
         blue = settings.value(key + "b")
         settings.endGroup()
-        return(QColor(red, green, blue))
+        return QColor(red, green, blue)
 
-    def set_loaded_color(self, key, index, button):
+    def set_loaded_color(self,key,index,button): 
         color = self.load_color(key)
         self.color_list[index] = color
         css = "background-color:" + color.name()
         button.setStyleSheet(css)
-
-
 
     def load_color_button_clicked(self):
         self.set_loaded_color("button_1", 0, self.button_1)
@@ -136,6 +97,11 @@ class Widget(QWidget, Ui_Widget):
         self.set_loaded_color("button_4", 3, self.button_4)
         self.set_loaded_color("button_5", 4, self.button_5)
         self.set_loaded_color("button_6", 5, self.button_6)
-        self.set_loaded_color("button_7", 6, self.button_7)
-        self.set_loaded_color("button_8", 7, self.button_8)
-        self.set_loaded_color("button_9", 8, self.button_9)
+
+    def save_color_button_clicked(self):
+        self.save_color("button_1", self.color_list[0])
+        self.save_color("button_2", self.color_list[1])
+        self.save_color("button_3", self.color_list[2])
+        self.save_color("button_4", self.color_list[3])
+        self.save_color("button_5", self.color_list[4])
+        self.save_color("button_6", self.color_list[5])
