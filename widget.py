bb6ef1aa0ee212b2c3266f855181014b0c7ba28c
NikNator10 <niknator1001.@gmail.com>
2023-09-06 23:08:47 +0200

    Wiederholung: Dateien mit QFile kopieren, QMessageBox, Styles anwenden; Genutzt: QApplication, QWidget, QHBoxLayout, QVBoxLayout, QLineEdit, QPushButton, QFileDialog, QMessageBox, QFile

--- widget.py
+++ widget.py
@@ -1,51 +1,47 @@
-from PySide6.QtWidgets import QWidget, QFileDialog, QPushButton, QVBoxLayout, QHBoxLayout, QTextEdit
-from PySide6.QtCore import QFile, QTextStream, QIODevice
+from PySide6.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QLineEdit, QPushButton, QFileDialog, QMessageBox
+from PySide6.QtCore import QFile
 
 class Widget(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Save & Open Files")
+        self.setWindowTitle("Select & Copy files")
+        self.resize(400,100)
 
-        self.text_edit = QTextEdit()
-        save_file_button = QPushButton("Save File")
-        open_file_button = QPushButton("Open File")
+        self.src_line_edit = QLineEdit()
+        self.dst_line_edit = QLineEdit()
 
-        v_button_layout = QVBoxLayout()
-        v_button_layout.addWidget(save_file_button)
-        v_button_layout.addWidget(open_file_button)
+        select_file_button = QPushButton("Select Button")
+        copy_file_button = QPushButton("Copy File")
 
-        h_layout = QHBoxLayout()
-        h_layout.addWidget(self.text_edit)
-        h_layout.addLayout(v_button_layout)
-        self.setLayout(h_layout)
+        h_src_layout = QHBoxLayout()
+        h_src_layout.addWidget(self.src_line_edit)
+        h_src_layout.addWidget(select_file_button)
 
-        save_file_button.clicked.connect(self.save_file)
-        open_file_button.clicked.connect(self.open_file)
+        h_dst_layout = QHBoxLayout()
+        h_dst_layout.addWidget(self.dst_line_edit)
+        h_dst_layout.addWidget(copy_file_button)
 
-    def save_file(self):
-        file_name,_ = QFileDialog.getSaveFileName(self, "Save File", "", "Text-File (*.txt);;All Files (*.*)")
-        if file_name == '':
+        v_layout = QVBoxLayout()
+        v_layout.addLayout(h_src_layout)
+        v_layout.addLayout(h_dst_layout)
+        self.setLayout(v_layout)
+
+        select_file_button.clicked.connect(self.select_file)
+        copy_file_button.clicked.connect(self.copy_file)
+
+    def select_file(self):
+        file_name,_ = QFileDialog.getOpenFileName(self, "Choose a file", "", "Text-File (*.txt);;All Files (*.*)")
+        if file_name == "":
             return
-        file = QFile(file_name)
-        if not file.open(QIODevice.WriteOnly | QIODevice.Text):
-            return
-        out_stream = QTextStream(file)
-        out_stream << self.text_edit.toPlainText()
+        self.src_line_edit.setText(file_name)
 
-    def open_file(self):
-        file_content = ''
-        file_name,_ = QFileDialog.getOpenFileName(self, "Open File", "", "Text-File (*.txt);;All Files (*.*)")
-        if file_name == '':
+    def copy_file(self):
+        src = self.src_line_edit.text()
+        dst = self.dst_line_edit.text()
+        if src == "" or dst == "":
             return
-        file = QFile(file_name)
-        if not file.open(QIODevice.ReadOnly | QIODevice.Text):
-            return
-        in_stream = QTextStream(file)
-        while not in_stream.atEnd():
-            line = in_stream.readLine()
-            file_content += line
-            file_content += '\n'
-        self.text_edit.clear()
-        self.text_edit.setText(file_content)
-        
-        
+        file = QFile(src)
+        if file.copy(dst):
+            QMessageBox.information(self, "Success", "File copied successfully")
+        else:
+            QMessageBox.information(self, "Error", "Could not copy file")
