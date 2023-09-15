8c62a32c9619d74cffad62c10f14ea70dd4166cc
NikNator10 <niknator1001.@gmail.com>
2023-09-15 18:59:23 +0200

    Widerholung: Networking in Qt-Applikationen einbauen; Genutzt: QApplication, QWidget, QByteArray, QUrl, QNetworkAccessManager, QNetworkRequest, QNetworkReply, Qt Designer

--- widget.py
+++ widget.py
@@ -1,31 +1,31 @@
 from PySide6.QtWidgets import QWidget
+from PySide6.QtNetwork import QNetworkAccessManager, QNetworkReply, QNetworkRequest
 from PySide6.QtCore import QByteArray, QUrl
-from PySide6.QtNetwork import QNetworkAccessManager, QNetworkRequest, QNetworkReply
 from ui_widget import Ui_Widget
 
 class Widget(QWidget, Ui_Widget):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.setWindowTitle("Basic Networking with QT")
+        self.setWindowTitle("Netowrking in Qt")
 
-        self.manager = QNetworkAccessManager(self)
-        self.data_buffer = QByteArray()
+        self.network_access_manager = QNetworkAccessManager(self)
+        self.data = QByteArray()
         self.request = QNetworkRequest()
 
-        self.request.setUrl(QUrl("https://github.com"))
+        self.request.setUrl(QUrl("https://google.com"))
 
-        self.net_reply = self.manager.get(self.request)
-        self.net_reply.readyRead.connect(self.data_ready_to_read)
-        self.net_reply.finished.connect(self.data_read_finished)
+        self.net_reply = self.network_access_manager.get(self.request)
+        self.net_reply.readyRead.connect(self.data_ready)
+        self.net_reply.finished.connect(self.finished)
+
+    def data_ready(self):
+        print("Data recieved")
+        self.data.append(self.net_reply.readAll())
     
-    def data_ready_to_read(self):
-        print("Data available")
-        self.data_buffer.append(self.net_reply.readAll())
-    
-    def data_read_finished(self):
+    def finished(self):
         print("Data read finished")
         if self.net_reply.error() is not QNetworkReply.NoError:
             print("Some error occurred")
         else:
-            self.textEdit.setText(str(self.data_buffer))
+            self.textEdit.setText(str(self.data))
