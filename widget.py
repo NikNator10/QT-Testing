fa1b10f62e80f0b95f5f52f15b89548acb332f87
NikNator10 <niknator1001.@gmail.com>
2023-09-16 23:29:47 +0200

    Widerholung: Networking in Qt-Applikationen einbauen; Genutzt: QApplication, QWidget, QByteArray, QUrl, QNetworkAccessManager, QNetworkRequest, QNetworkReply, QVBoxLayout, QTextEdit

--- widget.py
+++ widget.py
@@ -1,31 +1,34 @@
-from PySide6.QtWidgets import QWidget
-from PySide6.QtNetwork import QNetworkAccessManager, QNetworkReply, QNetworkRequest
-from PySide6.QtCore import QByteArray, QUrl
-from ui_widget import Ui_Widget
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QTextEdit
+from PySide6.QtCore import QUrl, QByteArray
+from PySide6.QtNetwork import QNetworkAccessManager, QNetworkRequest, QNetworkReply
 
-class Widget(QWidget, Ui_Widget):
+class Widget(QWidget):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
-        self.setWindowTitle("Netowrking in Qt")
+        self.setWindowTitle("Basic Networking in Qt")
 
-        self.network_access_manager = QNetworkAccessManager(self)
-        self.data = QByteArray()
+        self.text_edit = QTextEdit()
+        v_layout = QVBoxLayout()
+        v_layout.addWidget(self.text_edit)
+        self.setLayout(v_layout)
+
+        self.manager = QNetworkAccessManager(self)
+        self.data_buffer = QByteArray()
         self.request = QNetworkRequest()
 
         self.request.setUrl(QUrl("https://google.com"))
 
-        self.net_reply = self.network_access_manager.get(self.request)
-        self.net_reply.readyRead.connect(self.data_ready)
+        self.net_reply = self.manager.get(self.request)
+        self.net_reply.readyRead.connect(self.file_read_ready)
         self.net_reply.finished.connect(self.finished)
 
-    def data_ready(self):
+    def file_read_ready(self):
         print("Data recieved")
-        self.data.append(self.net_reply.readAll())
+        self.data_buffer.append(self.net_reply.readAll())
     
     def finished(self):
         print("Data read finished")
         if self.net_reply.error() is not QNetworkReply.NoError:
             print("Some error occurred")
         else:
-            self.textEdit.setText(str(self.data))
+            self.text_edit.setText(str(self.data_buffer))
