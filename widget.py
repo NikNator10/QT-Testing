d337fd436a4cdae63f47e95d423f3c48e658cacf
NikNator10 <niknator1001.@gmail.com>
2023-09-18 20:25:03 +0200

    Widerholung: Networking in Qt-Applikationen einbauen, InputDialog einbauen für URL; Genutzt: QApplication, QWidget, QVBoxLayout, QTextEdit, QInputDialog, QUrl, QByteArray, QNetworkAccessManager, QNetworkRequest, QNetworkReply

--- widget.py
+++ widget.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QTextEdit
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QTextEdit, QInputDialog
 from PySide6.QtCore import QUrl, QByteArray
 from PySide6.QtNetwork import QNetworkAccessManager, QNetworkRequest, QNetworkReply
 
@@ -12,22 +12,25 @@ class Widget(QWidget):
         v_layout.addWidget(self.text_edit)
         self.setLayout(v_layout)
 
+
         self.manager = QNetworkAccessManager(self)
         self.data_buffer = QByteArray()
         self.request = QNetworkRequest()
 
-        self.request.setUrl(QUrl("https://github.com"))
+        url,ok = QInputDialog.getText(self, "URL", "Enter URL:")
+        if ok and not url == "":
+            self.request.setUrl(QUrl(url))
 
         self.net_reply = self.manager.get(self.request)
-        self.net_reply.readyRead.connect(self.data_ready_read)
-        self.net_reply.finished.connect(self.data_load_finished)
-
-    def data_ready_read(self):
+        self.net_reply.readyRead.connect(self.data_read_ready)
+        self.net_reply.finished.connect(self.data_read_finished)
+    
+    def data_read_ready(self):
         print("Data recieved")
         self.data_buffer.append(self.net_reply.readAll())
 
-    def data_load_finished(self):
-        print("Data load finished")
+    def data_read_finished(self):
+        print("Data read finished")
         if self.net_reply.error() is not QNetworkReply.NoError:
             print("Some error occured")
         else:
